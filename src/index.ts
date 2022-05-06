6fb89148e4814a3ae190066392d9c054f21108f0
josebuschiazzo2 <104038315+josebuschiazzo2@users.noreply.github.com>
2022-05-06 15:18:55 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,20 @@ número es par o impar y le informe al usuario
 • En el caso de ser 0 (cero) el algoritmo deberá
 informarlo*/
 
-let num : number = Number (prompt("Ingrese el numero a evaluar"));
-if (num===0) {
-  console.log("Es el número 0, el cual no se puede determinar");
-  } else {
-      if (num%2===0) { 
-        console.log ("Su número es Par");
-      } else {
+let greater_zero:boolean=true;
+
+console.clear();
+while (greater_zero){
+  let num : number = Number (prompt("Ingrese el numero a evaluar"));
+  // pregunto primero si introdujo números, entero y si es mayor a 0
+  if (isNaN(num) || (num%1) || (num===0)){
+    alert("solos entero mayor a 0");
+    } else {
+        greater_zero=false;
+        if (num%2===0) { 
+          console.log ("Su número es Par");
+          } else {
          console.log ("Su número es impar");
+              }
         }
   }
